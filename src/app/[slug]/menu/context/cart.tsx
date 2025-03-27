557d3d30d0928a96fa7141489153454d8c999f47
Fabian Henrick <fabianhenrick@gmail.com>
2025-03-27 16:21:24 -0300

    feat: ensure same products be added more then one with quantity

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Product } from "@prisma/client";
-import { Children, createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
@@ -11,27 +11,42 @@ export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   toggleCart: () => void;
-  addProduct: (product:CartProduct) => void;
+  addProduct: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
-  addProduct: () => {}
+  addProduct: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-const addProduct = (product : CartProduct) => {
-setProducts(prev =>[...prev, product])
-}
+
   const toggleCart = () => {
     setIsOpen((prev) => !prev);
   };
+
+  const addProduct = (product: CartProduct) => {
+   {/* Verifica se o produto já está no carrinho */}
+    const productIsAlreadyOnTheCart = products.some((item) => item.id === product.id);
+
+    if (!productIsAlreadyOnTheCart) {
+      setProducts((prev) => [...prev, product]);
+      return;
+    }
+{/* Atualiza a quantidade do produto caso já esteja no carrinho */}
+    setProducts((prevProducts) =>
+      prevProducts.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + product.quantity } : item
+      )
+    );
+  };
+
   return (
-    <CartContext.Provider value={{ isOpen, products, toggleCart , addProduct}}>
+    <CartContext.Provider value={{ isOpen, products, toggleCart, addProduct }}>
       {children}
     </CartContext.Provider>
   );
